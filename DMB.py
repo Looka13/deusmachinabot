f0fb15c9074dfc979b0a287315cfff179fb57ccc
Looka13 <luca.inver@libero.it>
2019-02-19 20:04:39 +0100

    Experimenting with Psycopg2

--- DMB.py
+++ DMB.py
@@ -1,18 +1,33 @@
 ﻿import os
 import telegram
+import psycopg2
 import random
 from telegram.ext import MessageHandler, Filters, CommandHandler
 from datetime import datetime
 
 TOKEN = os.environ.get('TOKEN')
 PORT = int(os.environ.get('PORT', '8443'))
+ADMIN_ID = int(os.environ.get('ADMIN_ID'))
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
 def sigterm_handler(signum, frame):
-	updater.bot.send_message(chat_id=70649696, text="SIGTERM signal received.")
+	updater.bot.send_message(chat_id=ADMIN_ID, text="SIGTERM signal received.")
 
 updater = telegram.ext.Updater(token=TOKEN, user_sig_handler=sigterm_handler)
 dispatcher = updater.dispatcher
 random.seed()
+
+try:
+	conn = psycopg2.connect(DATABASE_URL, sslmode="require")
+	cursor = conn.cursor()
+	cursor.execute("CREATE TABLE IF NOT EXISTS memory (id INT PRIMARY KEY, name TEXT NOT NULL, value TEXT);"))
+	conn.commit()
+except:
+	pass
+finally:
+	if (conn):
+		cursor.close()
+		conn.close()
 
 def processText(bot, update):
 	s = random.randint(0, 3)
@@ -33,11 +48,52 @@ def processText(bot, update):
 def start(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text="The bot is not working yet!")
 
+def save(bot, update, args):
+	try:
+		conn = psycopg2.connect(DATABASE_URL, sslmode="require")
+		cursor = conn.cursor()
+		cursor.execute("SELECT value FROM memory WHERE name = prova;"))
+		result = cursor.fetchone()
+		if (result):
+			cursor.execute("UPDATE memory SET value = %s WHERE id = %s;", (str(args[0]), result[0]))
+		else:
+			cursor.execute("INSERT INTO memory (name, value) VALUES (%s, %s);", ("prova", str(args[0])))
+		conn.commit()
+		bot.send_message(chat_id=update.message.chat_id, text="Value saved.")
+	except:
+		bot.send_message(chat_id=update.message.chat_id, text="Saving was unsuccessful.")
+	finally:
+		if (conn):
+			cursor.close()
+			conn.close()
+
+def retrieve(bot, update):
+	try:
+		conn = psycopg2.connect(DATABASE_URL, sslmode="require")
+		cursor = conn.cursor()
+		cursor.execute("SELECT value FROM memory WHERE name = prova;"))
+		result = cursor.fetchone()
+		if (result):
+			if (result[2]):
+				bot.send_message(chat_id=update.message.chat_id, text="Retrieved value: {0}".format(result[2]))
+		else:
+			bot.send_message(chat_id=update.message.chat_id, text="No value saved.")
+	except:
+		bot.send_message(chat_id=update.message.chat_id, text="An error occured during retrieving.")
+	finally:
+		if (conn):
+			cursor.close()
+			conn.close()
+	
 # add handlers
 msg_handler = MessageHandler(Filters.text, processText)
 dispatcher.add_handler(msg_handler)
 start_handler = CommandHandler("start", start)
 dispatcher.add_handler(start_handler)
+save_handler = CommandHandler("save", save, pass_args=True)
+dispatcher.add_handler(save_handler)
+retrieve_handler = CommandHandler("retrieve", retrieve)
+dispatcher.add_handler(retrieve_handler)
 
 updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
 #updater.bot.set_webhook("https://deusmachinabot.herokuapp.com/"+TOKEN)
